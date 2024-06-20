8d429d134d2fd6e92e122e7fa55e18e2c01eaee1
Ashish Nallana <ashishnallana@gmail.com>
2024-06-20 17:49:39 +0530

    updating origins

--- app.js
+++ app.js
@@ -4,14 +4,32 @@ const userRouter = require("./Router/userRouter");
 const statusRouter = require("./Router/statusRouter");
 const cors = require("cors");
 
-// app.use(cors());
-app.use(
-  cors({
-    origin: ["http://localhost:5173"],
-    methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
-    credentials: true,
-  })
-);
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mykittylove.vercel.app",
+  "https://kittylove-git-main-ashishnallanas-projects.vercel.app",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
+  credentials: true,
+};
+
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173"],
+//     methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 
 app.use("/user", userRouter);
