f185d51c7971d2ae558207c297348c9c7855e3e0
Ashish Nallana <ashishnallana@gmail.com>
2024-06-27 19:20:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,32 +4,32 @@ const userRouter = require("./Router/userRouter");
 const statusRouter = require("./Router/statusRouter");
 const cors = require("cors");
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://mykittylove.vercel.app",
-  "https://kittylove-git-main-ashishnallanas-projects.vercel.app",
-];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://mykittylove.vercel.app",
+//   "https://kittylove-git-main-ashishnallanas-projects.vercel.app",
+// ];
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.includes(origin) || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
+//   credentials: true,
+// };
 
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173"],
-//     methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
-//     credentials: true,
-//   })
-// );
+// app.use(cors());
+app.use(
+  cors({
+    origin: ["https://mykittylove.vercel.app"],
+    methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/user", userRouter);
